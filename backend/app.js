a5410942dc2c26c8d679de0fb8a8e9eface17922
A-Normal-Guy-Inder <isb.inder59433@gmail.com>
2025-07-27 22:32:43 +0530

    Added port env

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");              
 const mongoose = require("mongoose");
 const app = express();                           
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const categoryRoutes = require("./routes/category");
 const brandRoutes = require ("./routes/brand");
