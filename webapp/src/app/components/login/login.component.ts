c7647813a929a03fa26410a6322b3ae1fa4201aa
A-Normal-Guy-Inder <isb.inder59433@gmail.com>
2025-07-29 13:38:11 +0530

    added demo credentials

--- webapp/src/app/components/login/login.component.ts
+++ webapp/src/app/components/login/login.component.ts
@@ -3,12 +3,12 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule,ReactiveFormsModule,NgIf],
+  imports: [MatInputModule,ReactiveFormsModule,NgIf,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
